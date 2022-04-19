48c339fa66649230bdb998ad31f7a39bdf923972
Corina Arreguez <coriarreguez@gmail.com>
2022-04-19 09:54:42 -0300

    BaSP-W05-2022: ex03-arrays-corrections

--- Semana-05/exercises/03_arrays.js
+++ Semana-05/exercises/03_arrays.js
@@ -3,41 +3,41 @@ console.log ('--EXERCISE 3: ARRAYS');
 //a. Dado el siguiente array: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", 
 //"Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"] mostrar por consola los meses 
 //5 y 11 (utilizar console.log).
-console.log ('-Exercise 2.a');
+console.log ('-Exercise 3.a');
 var meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 
 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
 console.log ('Mes 5: ' + meses[5] + ' / ' + 'Mes 11: ' + meses[11]);
 
 //b. Ordenar el array de meses alfabéticamente y mostrarlo por consola (utilizar sort).
-console.log ('-Exercise 2.b');
+console.log ('-Exercise 3.b');
 ordenAlf = meses.sort();
 console.log (ordenAlf);
 
 //c. Agregar un elemento al principio y al final del array (utilizar unshift y push).
-console.log ('-Exercise 2.c');
+console.log ('-Exercise 3.c');
 meses.unshift('Hola');
 meses.push('Adiós');
 console.log (meses);
 
 //d. Quitar un elemento del principio y del final del array (utilizar shift y pop).
-console.log ('-Exercise 2.d');
+console.log ('-Exercise 3.d');
 meses.shift();
 meses.pop();
 console.log(meses);
 
 //e. Invertir el orden del array (utilizar reverse).
-console.log ('-Exercise 2.e');
+console.log ('-Exercise 3.e');
 invertir = meses.reverse();
 console.log (invertir);
 
 //f. Unir todos los elementos del array en un único string donde cada mes este separado
 //por un guión - (utilizar join).
-console.log ('-Exercise 2.f');
+console.log ('-Exercise 3.f');
 console.log (meses.join('-'));
 
 //g. Crear una copia del array de meses que contenga desde Mayo hasta Noviembre (utilizar 
 //slice).
-console.log ('-Exercise 2.g');
+console.log ('-Exercise 3.g');
 var meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 
 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
 mesesCopia = meses.slice(4,10);
